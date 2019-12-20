9e95ebbd141470eaad754e87a9bfdc0c040ae099
devJRL <dev2sponge@gmail.com>
2019-12-20 11:41:33 +0900

    Action Creator with Action Types

---
+++ src-02/counter-redux-app/src/actions/ActionTypes.js
@@ -0,0 +1,7 @@
+/*
+  Action의 종류들을 선언합니다.
+  불러오기 : import * as types from './ActionTypes'
+*/
+export const INCREMENT = "INCREMENT";
+export const DECREMENT = "DECREMENT";
+export const SET_COLOR = "SET_COLOR";

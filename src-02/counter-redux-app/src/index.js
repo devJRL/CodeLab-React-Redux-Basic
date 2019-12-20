75d7d453a91ab1484d86ffa035557e5901c5787f
devJRL <dev2sponge@gmail.com>
2019-12-20 11:50:54 +0900

    Store for subscribe functions

--- src-02/counter-redux-app/src/index.js
+++ src-02/counter-redux-app/src/index.js
@@ -3,4 +3,9 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
+// [Store]
+// ? Install : yarn add redux react-redux --save
+import { createStore } from "react-redux";
+import reducers from "./reducers";
+
 ReactDOM.render(<App />, document.getElementById("root"));

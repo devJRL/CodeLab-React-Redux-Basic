2e1de235d98063e31da1a3efba35c37049e0a0df
devJRL <dev2sponge@gmail.com>
2019-12-20 11:37:07 +0900

    Presntational Container : Counter div

---
+++ src-02/counter-redux-app/src/components/Counter.js
@@ -0,0 +1,39 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./Counter.css";
+
+// [Presentational Component] 카운터 구현
+const Counter = ({ number, color, onIncrement, onDecrement, onSetColor }) => {
+  return (
+    <div
+      className="Counter"
+      onClick={onIncrement}
+      onContextMenu={e => {
+        e.preventDefault();
+        onDecrement();
+      }}
+      onDoubleClick={onSetColor}
+      style={{ backgroundColor: color }}
+    >
+      {number}
+    </div>
+  );
+};
+
+// 기본 props 타입과 값 설정
+Counter.propTypes = {
+  number: PropTypes.number,
+  color: PropTypes.string,
+  onIncrement: PropTypes.func,
+  onDecrement: PropTypes.func,
+  onSetColor: PropTypes.func
+};
+Counter.defaultProps = {
+  number: 0,
+  color: "black",
+  onIncrement: () => console.warn("onIncrement not defined"),
+  onDecrement: () => console.warn("onDecrement not defined"),
+  onSetColor: () => console.warn("onSetColor not defined")
+};
+
+export default Counter;

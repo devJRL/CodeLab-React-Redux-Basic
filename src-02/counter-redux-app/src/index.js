d4cff642ff7be93e37252b76c04033a1df043bbc
devJRL <dev2sponge@gmail.com>
2019-12-20 12:35:46 +0900

    Add Provider(react-redux) with Store(redux)

--- src-02/counter-redux-app/src/index.js
+++ src-02/counter-redux-app/src/index.js
@@ -4,8 +4,18 @@ import "./index.css";
 import App from "./App";
 
 // [Store]
-// ? Install : yarn add redux react-redux --save
+// ? Install : yarn add redux --save
 import { createStore } from "react-redux";
 import reducers from "./reducers";
+// [Provider]
+// ? Install : yarn add react-redux --save
+import { Provider } from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(reducers);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
